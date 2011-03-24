2d56e77c03f31cc6817da804795d197205350f69
Weibin Sun <wbsun@flux.utah.edu>
2011-03-24 12:40:47 -0600

    change kgpu for gECB

---
+++ kgpu/gputils.h
@@ -0,0 +1,34 @@
+#ifndef __GPUTILS_H__
+#define __GPUTILS_H__
+
+#define csc(...) _cuda_safe_call(__VA_ARGS__, __FILE__, __LINE__)
+static cudaError_t _cuda_safe_call(cudaError_t e, const char *file, int line) {
+    if (e!=cudaSuccess) {
+	fprintf(stderr, "kgpu Error: %s %d %s\n",
+		file, line, cudaGetErrorString(e));
+	//cpu_cleanup();
+	//gpu_cleanup();
+	cudaThreadExit();
+	abort();
+    }
+    return e;
+}
+
+
+static void *alloc_dev_mem(unsigned long size) {
+    void *h;
+    csc( cudaMalloc(&h, size) );
+    return h;
+}
+
+static void free_dev_mem(void *p) {
+    csc( cudaFree(p) );
+}
+
+#define ah2dcpy(dst, src, sz, stream) \
+    cudaMemcpyAsync((void*)(dst), (void*)(src), (sz), cudaMemcpyHostToDevice, (stream))
+
+#define ad2hcpy(dst, src, sz, stream) \
+    cudaMemcpyAsync((void*)(dst), (void*)(src), (sz), cudaMemcpyDeviceToHost, (stream))
+
+#endif

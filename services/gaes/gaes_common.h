f86b97d2c581ae99a78fc342d53165d8c0312477
Weibin Sun <wbsun@flux.utah.edu>
2011-05-02 15:01:15 -0600

    new common file for aes

---
+++ services/gaes/gaes_common.h
@@ -0,0 +1,20 @@
+/* This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the GPL-COPYING file in the top-level directory.
+ *
+ * Copyright (c) 2010-2011 University of Utah and the Flux Group.
+ * All rights reserved.
+ *
+ * KGPU GAES common header
+ */
+
+#ifndef __GAES_COMMON_H__
+#define __GAES_COMMON_H__
+
+struct gaes_ctr_info {
+	u32 key_enc[AES_MAX_KEYLENGTH_U32];
+	u32 key_dec[AES_MAX_KEYLENGTH_U32];
+	u32 key_length;
+	
+};
+
+#endif
